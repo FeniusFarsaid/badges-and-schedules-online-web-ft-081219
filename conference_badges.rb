6bb6e153850338ee2177595c754f0e20788a848d
David Ingram <david@ingramaviation.com>
2019-08-14 16:52:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,12 +5,18 @@ end
 
 
 
-
-# Write your code here.
-def batch_badge_creator(speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
-  badges = []
-  speakers.each do |name| 
-    badges << puts "Hello my name is #{name}."
-    badges
-  end 
+def batch_badge_creator(names)
+  names.collect {|name| badge_maker(name)}
 end
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+batch_badge_creator(names)
+# # Write your code here.
+# def batch_badge_creator(speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+#   badges = []
+#   speakers.each do |name| 
+#     badges << puts "Hello my name is #{name}."
+#     badges
+#   end 
+# end
