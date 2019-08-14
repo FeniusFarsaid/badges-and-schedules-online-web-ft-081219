278f7cca205a81e64ff33d0b5e41b847dd3b8e9f
David Ingram <david@ingramaviation.com>
2019-08-14 16:46:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ end
 # Write your code here.
 def batch_badge_creator(speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
   badges = []
-  speakers.each{|name| badges << 
-    puts "Hello my name is #{name}."
+  speakers.each do |name| 
+    badges << return "Hello my name is #{name}."
     badges
   
 end
