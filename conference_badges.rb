0445c10c035c5085571d932dd44574f2d987e029
David Ingram <david@ingramaviation.com>
2019-08-14 17:01:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,13 +14,9 @@ end
 
 
 
-# def batch_badge_creator(names)
-#   names.collect {|name| badge_maker(name)}
-# end
 
-# names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-# batch_badge_creator(names)
+
 
 
 
